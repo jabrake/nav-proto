a77c10ac1d90bea47d9ba38f785621dbec6a972e
Jorge Brake <jorge.brake@gmail.com>
2022-01-21 12:53:05 -0800

    Added scripts, updated gitignore, changed boilerplate code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+// import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class App extends React.Component {
+  constructor (props) {
+    super (props);
+
+    this.state = {
+      testState = '';
+    };
+  }
+
+  componentDidMount() {
+    console.log('App mounted!');
+  }
+
+  render() {
+    return (
+      <div>
+        Hello! App mounted!
+      </div>
+    )
+  }
 }
 
 export default App;
